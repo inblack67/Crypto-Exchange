fe4020069c3856fbf2a891f23aea795a41c7e48e
ryuzaki <ryuzaki>
2021-07-11 09:39:59 +0530

    token contract

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,13 @@
 const CryptoExchange = artifacts.require('CryptoExchange');
+const Token = artifacts.require('Token');
 
-module.exports = function (deployer) {
-  deployer.deploy(CryptoExchange);
+module.exports = async function (deployer) {
+  await deployer.deploy(Token);
+  const token = await Token.deployed();
+
+  await deployer.deploy(CryptoExchange);
+  const cryptoExchange = await CryptoExchange.deployed();
+
+  // transfer all funds to cryptoExchange
+  await token.transfer(cryptoExchange.address, '1000000000000000000000000');
 };
--- readme.md
+++ readme.md
@@ -1,1 +1,5 @@
 ### Crypto Exchange
+
+- Ethereum allows you creating your own crypto currency without creating your own blockchain
+- Smart contracts can be written to govern a crypto currency => ERC020 Tokens
+- ERC-20 => standard of all ethereum tokens
