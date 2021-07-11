e5a1bbb09d51655d964d8ed52761ebbc887b0023
ryuzaki <ryuzaki>
2021-07-11 23:00:24 +0530

    token test init

--- test/CryptoExchange.test.js
+++ test/CryptoExchange.test.js
@@ -4,6 +4,14 @@ const CryptoExchange = artifacts.require('CryptoExchange');
 require('chai').use(require('chai-as-promised')).should();
 
 contract('CryptoExchange', (accounts) => {
+  describe('Token deployment', async () => {
+    it('Token has a name', async () => {
+      const token = await Token.new();
+      const name = await token.name();
+      assert.equal(name, 'Crypto Token');
+    });
+  });
+
   describe('CryptoExchange deployment', async () => {
     it('Contract has a name', async () => {
       const cryptoExchange = await CryptoExchange.new();
