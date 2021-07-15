fb4af8a804606b1633a7d428bd328ddfd9dd7a21
ryuzaki <ryuzaki>
2021-07-15 23:19:44 +0530

    test suite cont.

--- readme.md
+++ readme.md
@@ -3,3 +3,4 @@
 - Ethereum allows you creating your own crypto currency without creating your own blockchain
 - Smart contracts can be written to govern a crypto currency => ERC020 Tokens
 - ERC-20 => standard of all ethereum tokens
+- **Wei** => smallest unit of ethereum token => like cents
--- test/CryptoExchange.test.js
+++ test/CryptoExchange.test.js
@@ -1,12 +1,24 @@
+const { assert } = require('chai');
+
 const Token = artifacts.require('Token');
 const CryptoExchange = artifacts.require('CryptoExchange');
 
 require('chai').use(require('chai-as-promised')).should();
 
+const getEthers = (amount) => web3.utils.toWei(amount.toString(), 'ether');
+
+let token;
+let cryptoExchange;
+
 contract('CryptoExchange', (accounts) => {
+  before(async () => {
+    token = await Token.new();
+    cryptoExchange = await CryptoExchange.new();
+    await token.transfer(cryptoExchange.address, getEthers(1000000)); // 1 million
+  });
+
   describe('Token deployment', async () => {
     it('Token has a name', async () => {
-      const token = await Token.new();
       const name = await token.name();
       assert.equal(name, 'Crypto Token');
     });
@@ -14,9 +26,13 @@ contract('CryptoExchange', (accounts) => {
 
   describe('CryptoExchange deployment', async () => {
     it('Contract has a name', async () => {
-      const cryptoExchange = await CryptoExchange.new();
       const name = await cryptoExchange.name();
       assert.equal(name, 'Crypto Exchange');
+    });
+
+    it('Contract has tokens', async () => {
+      const balance = await token.balanceOf(cryptoExchange.address);
+      assert.equal(balance.toString(), getEthers(1000000));
     });
   });
 });
